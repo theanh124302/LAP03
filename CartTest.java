460ead26d02ae8a7889c82b438c8775c2dad3bc5
Theanh <hoangtheanh200hhbg@gmail.com>
2022-11-26 22:36:22 +0700

    a

---
+++ CartTest.java
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+package LAB03;//HoangTheAnh20204938  
+
+public class CartTest {
+
+	public static void main(String[] args) {
+		Cart cart = new Cart();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers", 87, 24.95f);
+		cart.addDVD(dvd1);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction", "George Lucas", 87, 19.95f);
+		cart.addDVD(dvd2);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+		cart.addDVD(dvd3);
+		cart.showCart();
+	}
+=======
+package LAB03;//HoangTheAnh20204938  
+
+public class CartTest {
+
+	public static void main(String[] args) {
+		Cart cart = new Cart();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers", 87, 24.95f);
+		cart.addDVD(dvd1);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction", "George Lucas", 87, 19.95f);
+		cart.addDVD(dvd2);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+		cart.addDVD(dvd3);
+		cart.showCart();
+	}
+>>>>>>> 13e8c54cef72d8312b5ca2e7e1f974775fd24a38
+}
